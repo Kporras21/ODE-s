75cbc5c7d5e391c3b341af94c8455011243a3710
DylanNB1 <dylan.navarro@ucr.ac.cr>
2024-06-21 23:18:59 -0600

    3 ejemplos de RK4

--- ode/ode.py
+++ ode/ode.py
@@ -41,15 +41,38 @@ def RK4(function, x_0, t, N):
     """Devuelve la solución númerica de una ODE de primer orden no homogenea
 
     Examples:
+
+        >>> import numpy as np
+        >>> import math
         >>> def function1(x,t):
                 return np.sin(t) - x**3
-
         >>> result1 = RK4(function2, 0, 10, 20)
-            print(result1)
+        >>> print(result1)
         [ 0.          0.13505937  0.48487586  0.81979626  0.93102462  0.88144889
         0.72370945  0.46026479  0.06848691 -0.42773679 -0.78084333 -0.83096351
         -0.6993985  -0.43991777 -0.04506765  0.45089026  0.79057875  0.83047637
         0.69376603  0.43014634]
+        >>> def falling_function(v, t):
+                k = 0.1       
+                m = 1.0       
+                return 9.8 - (k / m) * v
+        >>> result2 = RK4(function_falling, 0, 10, 20)
+        >>> print(result2)
+        [ 0.          5.02963768  9.80140324 14.32851792 18.62352509 22.69832502
+        26.56420785 30.23188487 33.7115182  37.01274893 40.14472385 43.1161208
+        45.93517269 48.60969034 51.14708409 53.55438434 55.83826107 58.00504226
+        60.06073146 62.01102441]
+        >>> def rc_circuit_function(Q, t):
+                V = 10.0     
+                R = 1.0      
+                C = 1.0      
+                return (V - Q / C) / R
+        >>> result3 = RK4(rc_circuit_function, 0, 10, 20)
+        >>> print(result3)
+        [0.         4.08913375 6.50616602 7.93484147 8.77931241 9.2784679
+        9.57351202 9.74790866 9.85099218 9.91192347 9.94793914 9.96922752
+        9.9818108  9.98924861 9.993645   9.99624364 9.99777967 9.99868759
+        9.99922425 9.99954147]
 
     Args:
         function (callable): La función complementaria de la ODE
