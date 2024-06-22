2efd80da8e529a02dac8d59809b93663f787ec91
DylanNB1 <dylan.navarro@ucr.ac.cr>
2024-06-21 22:28:43 -0600

    Prueba de ejemplos

--- ode/ode.py
+++ ode/ode.py
@@ -41,8 +41,15 @@ def RK4(function, x_0, t, N):
     """Devuelve la solución númerica de una ODE de primer orden no homogenea
 
     Examples:
-        >>> sum(3.0, 4.0)
-        7.0
+        >>> def function1(x,t):
+                return np.sin(t) - x**3
+
+        >>> result1 = RK4(function2, 0, 10, 20)
+            print(result1)
+        [ 0.          0.13505937  0.48487586  0.81979626  0.93102462  0.88144889
+        0.72370945  0.46026479  0.06848691 -0.42773679 -0.78084333 -0.83096351
+        -0.6993985  -0.43991777 -0.04506765  0.45089026  0.79057875  0.83047637
+        0.69376603  0.43014634]
 
     Args:
         function (callable): La función complementaria de la ODE
