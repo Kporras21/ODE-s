fdfb00cfbe2b05b9b14e0e0b844edf1870860cb0
DylanNB1 <dylan.navarro@ucr.ac.cr>
2024-06-21 22:21:14 -0600

    Correcion KR4

--- ode/ode.py
+++ ode/ode.py
@@ -40,6 +40,10 @@ def RK2(function, x_0, t, N):
 def RK4(function, x_0, t, N):
     """Devuelve la solución númerica de una ODE de primer orden no homogenea
 
+    Examples:
+        >>> sum(3.0, 4.0)
+        7.0
+
     Args:
         function (callable): La función complementaria de la ODE
         x_0 (float): Valor de la función solución en el valor inicial
@@ -59,7 +63,7 @@ def RK4(function, x_0, t, N):
         k2 = h * function(x[i] + k1/2, times[i] + h/2)
         k3 = h * function(x[i] + k2/2, times[i] + h/2)
         k4 = h * function(x[i] + k3, times[i] + h)
-        x[i+1] = x[i] + (1/6) * ( k1 + 2 * k2  * k3 + k4) 
+        x[i+1] = x[i] + (1/6) * (k1 + 2 * k2 + 2  * k3 + k4) 
     return times, x
 
 def f(x,t):
